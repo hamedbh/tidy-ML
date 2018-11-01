78d616be6dca6322a2367d379974b92186bea9da
Hamed Bastan-Hagh <hamedbh@radlabs-MacBook-Pro.local>
2018-11-01 15:33:52 +0000

    finished third chapter

--- tidy_ML_notes.R
+++ tidy_ML_notes.R
@@ -12,6 +12,7 @@ datacamp_names <- c("country", "year", "infant_mortality", "life_expectancy",
 setdiff(names(gapminder), datacamp_names)
 setdiff(datacamp_names, names(gapminder))
 d <- as_tibble(gapminder) %>% 
+    mutate(country = as.character(country)) %>% 
     inner_join(read_csv("data/gap_countries.csv"), 
                by = "country") %>% 
     mutate(country = as.factor(country)) %>% 
@@ -20,7 +21,12 @@ d <- as_tibble(gapminder) %>%
     select(-gdp) %>% 
     arrange(country)
     
-    
+# impute missing values
+library(caret)
+d <- predict(preProcess(d %>% 
+                            dplyr::select(-life_expectancy), 
+                        method = c("bagImpute")), 
+             d)
 d
 # can group the tibble bny country and then nest those as a list column
 gap_nested <- d %>% 
@@ -113,3 +119,95 @@ fullmodel_perf %>%
     rename(multi_AIC = AIC) %>% 
     inner_join(worst_models, by = "country") %>% 
     select(country, single_AIC = AIC, multi_AIC)
+
+
+# Paritioning for train, validate, test -----------------------------------
+
+# Two questions to consider
+## 1. How well would my model perform on new data?
+## 2. Did I select the best-performing model?
+
+library(rsample)
+library(Metrics)
+gap_split <- initial_split(d, prop = 0.75)
+train_data <- training(gap_split)
+test_data <- testing(gap_split)
+
+# further split for CV
+cv_folds <- vfold_cv(d, v = 10)
+cv_folds
+# extract the train and test set for each fold
+cv_data <- cv_folds %>% 
+    mutate(train = map(splits, ~ training(.x))) %>% 
+    mutate(validate = map(splits, ~ testing(.x)))
+cv_data
+cv_models_lm <- cv_data %>% 
+    mutate(model = map(train, ~ lm(life_expectancy ~ ., data = .x)))
+
+# generate predictions on the validation sets
+cv_prep_lm <- cv_models_lm %>% 
+    mutate(validate_actual = map(validate, ~ .x$life_expectancy), 
+           validate_predicted = map2(model, 
+                                     validate, 
+                                     ~ predict(.x, .y)))
+
+# calculate an error metric, in this case MAE
+cv_eval_lm <- cv_prep_lm %>% 
+    mutate(validate_mae = map2_dbl(validate_actual, 
+                                   validate_predicted, 
+                                   ~ mean(abs(.x - .y), na.rm = TRUE)))
+
+
+# Random Forest -----------------------------------------------------------
+
+library(ranger)
+cv_models_rf <- cv_data %>% 
+    mutate(model = map(train, ~ ranger(life_expectancy ~ ., 
+                                       data = .x, 
+                                       seed = 42)))
+cv_eval_rf <- cv_models_rf %>% 
+    mutate(validate_actual = map(validate, ~ .x$life_expectancy), 
+           validate_predicted = map2(model, 
+                                     validate, 
+                                     ~ predict(.x, .y)$predictions)) %>% 
+    mutate(validate_mae = map2_dbl(validate_actual, 
+                                   validate_predicted, 
+                                   ~ mean(abs(.x - .y), na.rm = TRUE)))
+cv_eval_rf
+
+# for hyperparameter tuning can use crossing() to get all combinations, as 
+# with expand.grid
+cv_tune <- cv_data %>% 
+    crossing(mtry = seq_along(1:6))
+
+cv_model_tunerf <- cv_tune %>% 
+    mutate(model = map2(train, 
+                        mtry, 
+                        ~ ranger(life_expectancy ~ ., 
+                                 data = .x, 
+                                 mtry = .y, 
+                                 seed = 42)))
+cv_eval_tunerf <- cv_model_tunerf %>% 
+    mutate(validate_actual = map(validate, ~ .x$life_expectancy), 
+           validate_predicted = map2(model, 
+                                     validate, 
+                                     ~ predict(.x, .y)$predictions)) %>% 
+    mutate(validate_mae = map2_dbl(validate_actual, 
+                                   validate_predicted, 
+                                   ~ mean(abs(.x - .y), na.rm = TRUE)))
+cv_eval_tunerf %>% 
+    group_by(mtry) %>% 
+    summarise(avg_mae = mean(validate_mae)) %>% 
+    arrange(avg_mae)
+
+# Now train the best model (ranger with mtry = 4) on the full training dataset, 
+# and then evaluate its performance on the test set
+
+best_model <- ranger(formula = life_expectancy ~ ., 
+                     data = train_data, 
+                     mtry = 4,
+                     num.trees = 100, 
+                     seed = 42)
+preds <- predict(best_model, test_data)
+test_mae <- mean(abs(preds$predictions - test_data$life_expectancy))
+test_mae
